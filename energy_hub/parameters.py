95b0ac9099d5bd3f69ba43a99164e72977e22a13
Zhao Tianyang <zhaoty@ntu.edu.sg>
2017-10-16 15:15:06 +0800

    Update the parameter settings of energy hub

--- energy_hub/parameters.py
+++ energy_hub/parameters.py
@@ -1,14 +1,22 @@
 #The parameters setting for the energy hub.
 T = 24
 
+# The parameter is obtained from
 eff_bat = 0.9
 Emax = 10
 Emin = 100
 E0 = 50
 
+# The parameter is obtained from
 Pbic_max = 100
 eff_bic = 0.95
 
+# The maximal power exchange between the microgrid and utility grid
 Pug_max = 100
 Pug_min = 0
 
+# The efficiency for the HVAC system
+eff_HVACh = 0.9
+eff_HVACc = 0.9
+
+#The heat, cooling and electrical demand curve
