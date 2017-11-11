a5a7d816cfa6cfb5f215d86d0790b2227003c0d0
Matrixeigs <matrixeigs@gmail.com>
2017-11-11 17:01:31 +0800

    Update power flow modelling

--- Two_stage_stochastic_optimization/main.py
+++ Two_stage_stochastic_optimization/main.py
@@ -1,11 +1,16 @@
 from Two_stage_stochastic_optimization.power_flow_modelling import idx_bus
 from os.path import dirname, join
+from pypower.loadcase import loadcase
 
 def optimal_power_flow(*args):
     casedata = args[0] # Target power flow modelling
+    mpc = loadcase(casedata) # Import the power flow modelling
 
 
+    return mpc
 
 if __name__=="__main__":
-    casedata = join(dirname(__file__), 'power_flow_modelling/case9')
+    from Two_stage_stochastic_optimization.power_flow_modelling.case30 import case30
+    casedata = case30()
     result = optimal_power_flow(casedata)
+    # print(result)
