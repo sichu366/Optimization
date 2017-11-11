72130008b17ffe4330414de49ce7468f01b76d1b
Matrixeigs <matrixeigs@gmail.com>
2017-11-11 19:17:49 +0800

    Update OPF modelling

--- Two_stage_stochastic_optimization/main.py
+++ Two_stage_stochastic_optimization/main.py
@@ -2,10 +2,16 @@ from Two_stage_stochastic_optimization.power_flow_modelling import idx_bus
 from os.path import dirname, join
 from pypower.loadcase import loadcase
 from pypower.ext2int import ext2int
-from numpy import zeros, c_, shape, ix_,ones
+from numpy import zeros, c_, shape, ix_,ones,r_,arange,sum
 from numpy import flatnonzero as find
-from pypower.idx_brch import F_BUS, T_BUS, BR_X, TAP, SHIFT, BR_STATUS
+from scipy.sparse.linalg import inv
 
+from pypower.idx_brch import F_BUS, T_BUS, BR_X, TAP, SHIFT, BR_STATUS,RATE_A
+from pypower.idx_cost import MODEL, NCOST, PW_LINEAR, COST, POLYNOMIAL
+from pypower.idx_bus import BUS_TYPE, REF, VA, VM, PD, GS, VMAX, VMIN
+from pypower.idx_gen import GEN_BUS, VG, PG, QG, PMAX, PMIN, QMAX, QMIN
+
+from scipy.sparse import csr_matrix as sparse
 def optimal_power_flow(*args):
     casedata = args[0] # Target power flow modelling
     mpc = loadcase(casedata) # Import the power flow modelling
@@ -17,11 +23,34 @@ def optimal_power_flow(*args):
     nl = shape(mpc['branch'])[0]  ## number of branches
     ng = shape(mpc['gen'])[0]  ## number of dispatchable injections
 
+    ## Formualte the
     stat = branch[:, BR_STATUS]  ## ones at in-service branches
     b = stat / branch[:, BR_X]  ## series susceptance
     tap = ones(nl)  ## default tap ratio = 1
     i = find(branch[:, TAP])  ## indices of non-zero tap ratios
     tap[i] = branch[i, TAP]  ## assign non-zero tap ratios
+
+    ## build connection matrix Cft = Cf - Ct for line and from - to buses
+    f = branch[:, F_BUS]  ## list of "from" buses
+    t = branch[:, T_BUS]  ## list of "to" buses
+    i = r_[range(nl), range(nl)]  ## double set of row indices
+    ## connection matrix
+    Cft = sparse((r_[ones(nl), -ones(nl)], (i, r_[f, t])), (nl, nb))
+
+    ## build Bf such that Bf * Va is the vector of real branch powers injected
+    ## at each branch's "from" bus
+    Bf = sparse((r_[b, -b], (i, r_[f, t])), shape=(nl, nb))  ## = spdiags(b, 0, nl, nl) * Cft
+
+    ## build Bbus
+    Bbus = Cft.T * Bf
+    # The distribution factor
+    Distribution_factor = Bf*inv(Bbus)
+
+    Cg = sparse((ones(ng), (gen[:, GEN_BUS], arange(ng))), (nb, ng)) # Sparse index generation method is different from the way of matlab
+
+    Pd = sum(bus[:,PD])# Total power demand
+
+
     return mpc
 
 if __name__=="__main__":
