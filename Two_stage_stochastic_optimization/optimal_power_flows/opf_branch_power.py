1dedd91f54294f3a8ae18f33541bae3f7276db53
Matrixeigs <matrixeigs@gmail.com>
2018-01-10 23:32:23 +0800

    Mode display in the logger

--- Two_stage_stochastic_optimization/optimal_power_flows/opf_branch_power.py
+++ Two_stage_stochastic_optimization/optimal_power_flows/opf_branch_power.py
@@ -5,6 +5,9 @@ Optimal power flow based on branch power flow modelling
 Additional case33 is added to the test cases
 
 Note: The proposed method has been verified
+
+@author: Tianyang Zhao
+@email: zhaoty@ntu.edu.sg
 """
 
 from Two_stage_stochastic_optimization.power_flow_modelling import case33
